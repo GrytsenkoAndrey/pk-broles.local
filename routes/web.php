471bea938dbde7a4b743e590da85e5e8f3d8a369
Andrey Grytsenko <andreyapg@gmail.com>
2022-10-23 11:42:33 +0300

    Clear  routes/web.php 2022-10-23

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware(['auth', 'verified'])
     ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+
         Route::prefix('student')
             ->name('student.')
             ->group(function () {
