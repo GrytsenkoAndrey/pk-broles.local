58829b780febbbce6c201036df25caf868b98104
Andrey Grytsenko <andreyapg@gmail.com>
2022-10-23 11:34:07 +0300

    Add Student Timetable (controller, view) 2022-10-23

---
+++ app/Http/Controllers/Student/TimetableController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class TimetableController extends Controller
+{
+    public function index(): View
+    {
+        return view('student.timetable');
+    }
+}
